bd102d149eb1bd8e0a183b0eeb958b658559a77c
Khateeb Ahmed Dar <141232264+Khateeb21@users.noreply.github.com>
2024-09-30 14:40:31 +0530

    changed some files
    change

---
+++ backend/config/dbConnect.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export const connectDatabase = () => {
+  let DB_URI = "";
+
+  if (process.env.NODE_ENV === "DEVELOPMENT") DB_URI = process.env.DB_LOCAL_URI;
+
+console.log("db_uri",DB_URI)
+mongoose.connect(DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then((con) => {
+
+  console.log(`MongoDB connected with host: ${con.connection.host}`);
+}).catch((err) => {
+
+  console.error('Error connecting to MongoDB:', err);}
+
+)}
